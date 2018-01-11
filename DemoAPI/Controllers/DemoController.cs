f4d843985dbb228b727f92495c0c2367f3d2679d
Saxena <psaxena@brandywineglobal.com>
2018-01-11 10:17:59 -0500

    added a controller.

---
+++ DemoAPI/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Web.Http;
+
+namespace DemoAPI.Controllers
+{
+    //[EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class DemoController : ApiController
+    {
+
+        [Route("api/user")]
+        [HttpGet]
+        public string GetWindowsUser()
+        {
+            var id = WindowsIdentity.GetCurrent();
+
+            return id.Name;
+        }
+    }
+}
