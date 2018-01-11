2ecae7d462f8bc63b5290b2cacffc0c657d2a528
Saxena <psaxena@brandywineglobal.com>
2018-01-11 09:52:56 -0500

    added arbitrary models

---
+++ DemoAPI/Models/Model1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DemoAPI.Models
+{
+    public class Model1
+    {
+        [Key]
+        public string keyString { get; set; }
+
+        public string attribute1 { get; set; }
+        public string attribute2 { get; set; }
+        public string attribute3 { get; set; }
+    }
+}
